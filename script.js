a288b7e1cae8f65db85d1115e4ed5e61849cbfa6
brianpompey <brianthescholar@gmail.com>
2021-03-23 14:14:35 -0400

    Added constants and functions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+
+const calendar = document.getElementById('calendar');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function load() {
+
+}
+
+load();
