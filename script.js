489fa1c4822122fd0146aa97cb97e0209d15c2d0
brianpompey <brianthescholar@gmail.com>
2021-03-23 15:09:18 -0400

    modal showing up now

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 
 const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function openModal(date) {
+    clicked = date;
+
+    const eventForDay = events.find(e => e.date == clicked);
+
+    if (eventForDay) {
+        console.log('Event exists')
+    } else {
+        newEventModal.style.display = 'block';
+    }
+
+    backDrop.style.display = 'block';
+}
 
 function load() {
     const dt = new Date();
@@ -40,7 +58,7 @@ function load() {
         if (i > paddingDays) {
             daySquare.innerText = i - paddingDays;
 
-            daySquare.addEventListener('click', () => console.log('click'));
+            daySquare.addEventListener('click', () => openModal(`${month + 1}/${i - paddingDays}/${year}`));
         } else {
             daySquare.classList.add('padding');
         }
